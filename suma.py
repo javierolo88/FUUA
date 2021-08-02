e3ed7b92ef4c7415a709eb9133f659aa7af4f7a6
javierolo88 <52211265+javierolo88@users.noreply.github.com>
2021-08-02 12:27:49 -0500

    primera vez
    priemra

---
+++ suma.py
@@ -0,0 +1,5 @@
+import random
+x=3
+t=6
+print ('hola "\bmundo": \n',x+t,"pepe" )
+it t.iss
---
+++ visual.py
@@ -0,0 +1,10 @@
+import random
+x=3
+t=6
+try:
+    f=int (input ("ingrese el numero: "))
+    k=t+f
+    print (k)
+except ValueError:
+    print("ingresa el valor numerico unicamente numeros ")    
+
